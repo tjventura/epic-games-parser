77faaa286199d22f33bfbd9d2552d3bb3dc70905
unknown <tiago.ventura12@gmail.com>
2020-05-15 20:36:18 +0100

    aa

--- epic_games_parser/parser.py
+++ epic_games_parser/parser.py
@@ -15,6 +15,9 @@ def fetcher():
     with urllib.request.urlopen("https://store-site-backend-static.ak.epicgames.com/freeGamesPromotions") as url:
         data = json.loads(url.read().decode())
 
+        print(data['data']['Catalog']['searchStore']['elements'][0]['keyImages'][0]['url'])
+        print(data['data']['Catalog']['searchStore']['elements'][1]['keyImages'][1]['url'])
+
         freeGamesData = data['data']['Catalog']['searchStore']['elements']
 
         i = 0
@@ -25,10 +28,12 @@ def fetcher():
                 #gamesInfo[]
                 gamesInfo[i]['startDate'] = AvailableTime['startDate']
                 gamesInfo[i]['endDate'] = AvailableTime['endDate']
+                gamesInfo[i]['thumbnail'] = freeGameInfo['keyImages'][0]['url']
             else:
                 AvailableTime = freeGameInfo['promotions']['promotionalOffers'][0]['promotionalOffers'][0]
                 gamesInfo[i]['startDate'] = AvailableTime['startDate']
                 gamesInfo[i]['endDate'] = AvailableTime['endDate']
+                gamesInfo[i]['thumbnail'] = freeGameInfo['keyImages'][1]['url']
             i+=1
         i = 0            
         return json.dumps(gamesInfo)
