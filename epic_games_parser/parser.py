d827654ca7d9b5c73bbf7790fa6d601edba24cd0
unknown <tiago.ventura12@gmail.com>
2020-05-15 19:40:53 +0100

    fixed a bug with the json file

--- epic_games_parser/parser.py
+++ epic_games_parser/parser.py
@@ -1,10 +1,15 @@
 import urllib.request, json 
 
-gamesInfo = {
+gamesInfo = [{
     'title': 'title',
     'startDate': 'startDate',
     'endDate': 'endDate'
-}
+},
+{
+    'title': 'title',
+    'startDate': 'startDate',
+    'endDate': 'endDate'
+},]
 
 def fetcher():
     with urllib.request.urlopen("https://store-site-backend-static.ak.epicgames.com/freeGamesPromotions") as url:
@@ -12,18 +17,20 @@ def fetcher():
 
         freeGamesData = data['data']['Catalog']['searchStore']['elements']
 
+        i = 0
         for freeGameInfo in freeGamesData:
-            gamesInfo['title'] = freeGameInfo['title']
+            gamesInfo[i]['title'] = freeGameInfo['title']
             if freeGameInfo['title'] == 'Mystery Game':
                 AvailableTime = freeGameInfo['promotions']['upcomingPromotionalOffers'][0]['promotionalOffers'][0]
                 #gamesInfo[]
-                gamesInfo['startDate'] = AvailableTime['startDate']
-                gamesInfo['endDate'] = AvailableTime['endDate']
+                gamesInfo[i]['startDate'] = AvailableTime['startDate']
+                gamesInfo[i]['endDate'] = AvailableTime['endDate']
             else:
                 AvailableTime = freeGameInfo['promotions']['promotionalOffers'][0]['promotionalOffers'][0]
-                gamesInfo['startDate'] = AvailableTime['startDate']
-                gamesInfo['endDate'] = AvailableTime['endDate']
-
+                gamesInfo[i]['startDate'] = AvailableTime['startDate']
+                gamesInfo[i]['endDate'] = AvailableTime['endDate']
+            i+=1
+        i = 0            
         return json.dumps(gamesInfo)
 
 fetcher()
