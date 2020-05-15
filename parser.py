df192d5d5efe139bb143310adc88bf22b37e75a0
unknown <tiago.ventura12@gmail.com>
2020-05-15 14:39:02 +0100

    parser complete, returning a json with the available free games and their availability

---
+++ parser.py
@@ -0,0 +1,29 @@
+import urllib.request, json 
+
+gamesInfo = {
+    'title': 'title',
+    'startDate': 'startDate',
+    'endDate': 'endDate'
+}
+
+def fetcher():
+    with urllib.request.urlopen("https://store-site-backend-static.ak.epicgames.com/freeGamesPromotions") as url:
+        data = json.loads(url.read().decode())
+
+        freeGamesData = data['data']['Catalog']['searchStore']['elements']
+
+        for freeGameInfo in freeGamesData:
+            gamesInfo['title'] = freeGameInfo['title']
+            if freeGameInfo['title'] == 'Mystery Game':
+                AvailableTime = freeGameInfo['promotions']['upcomingPromotionalOffers'][0]['promotionalOffers'][0]
+                #gamesInfo[]
+                gamesInfo['startDate'] = AvailableTime['startDate']
+                gamesInfo['endDate'] = AvailableTime['endDate']
+            else:
+                AvailableTime = freeGameInfo['promotions']['promotionalOffers'][0]['promotionalOffers'][0]
+                gamesInfo['startDate'] = AvailableTime['startDate']
+                gamesInfo['endDate'] = AvailableTime['endDate']
+
+            print(json.dumps(gamesInfo))
+
+fetcher()
